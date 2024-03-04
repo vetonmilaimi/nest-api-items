c57e11a80132cc45ea4fff120ce28b95639af782
vetonmilaimi <vetonmila@gmail.com>
2024-03-04 23:06:19 +0100

    Creating CRUD with relations for items and created structure of API

---
+++ src/database/database.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow('MYSQL_HOST'),
+        port: configService.getOrThrow('MYSQL_PORT'),
+        database: configService.getOrThrow('MYSQL_DATABASE'),
+        username: configService.getOrThrow('MYSQL_USERNAME'),
+        password: configService.getOrThrow('MYSQL_PASSWORD'),
+        autoLoadEntities: true,
+        // Not recommended for production
+        synchronize: configService.getOrThrow('MYSQL_SYNCHRONIZE'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
